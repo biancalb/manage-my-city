b30e464f6c87bea97b7ebf1f58f5d07893df40f7
Bianca <biancalbarbosa@hotmail.com>
2024-01-15 22:03:04 -0300

    Add validation for multiple errors and yup  translation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
 interface ICidade{
@@ -7,21 +8,25 @@ interface ICidade{
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
     nome: yup.string().required().min(3),
+    estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{},{},ICidade>, res: Response) => {
     let validatedData: ICidade | undefined = undefined;
 
     try {
-        validatedData = await bodyValidation.validate(req.body);
+        validatedData = await bodyValidation.validate(req.body, { abortEarly: false});
 
-    } catch (error) {
-        const yupError = error as yup.ValidationError;
-        return res.json({
-            erros: {
-                default: yupError.message,
-            }
+    } catch (err) {
+        const yupError = err as yup.ValidationError;
+        const errors: Record<string, string> = {};
+
+        yupError.inner.forEach(err => {
+            if (!err.path) return;
+            errors[err.path] = err.message;
         });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({ erros: errors });
     }
 
     console.log(validatedData);
