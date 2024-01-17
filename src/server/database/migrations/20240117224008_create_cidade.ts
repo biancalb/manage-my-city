32518ac0e8fe210e1710444b32b011f3c53a6c84
Bianca <biancalbarbosa@hotmail.com>
2024-01-17 20:07:29 -0300

    Add cidade migration

---
+++ src/server/database/migrations/20240117224008_create_cidade.ts
@@ -0,0 +1,23 @@
+import type { Knex } from 'knex';
+import { ETableNames } from '../ETableNames';
+
+export async function up(knex: Knex): Promise<void> {
+    return knex
+        .schema.createTable(ETableNames.cidade, table => {
+            table.bigIncrements('id').primary().index();
+            table.string('nome', 150).index().notNullable();
+
+            table.comment('Tabela usada para armazenar cidades');
+        })
+        .then(() => {
+            console.log(`# Table ${ETableNames.cidade} created`);
+        });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex
+        .schema.dropTable(ETableNames.cidade)
+        .then(() => {
+            console.log(`# Table ${ETableNames.cidade} dropped`);
+        });
+}
