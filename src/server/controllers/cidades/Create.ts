489604de93d473c4b0f18fa0dfa7753028edbc9d
Bianca <biancalbarbosa@hotmail.com>
2024-01-20 09:42:04 -0300

    Add cidade provider

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,11 +3,12 @@ import * as yup from 'yup';
 import { validation } from '../../shared/middleware/validations';
 import { StatusCodes } from 'http-status-codes';
 import { ICidade } from '../../database/models';
+import { CidadesProvider } from '../../database/providers/cidades';
 
 interface IBodyProps extends Omit<ICidade, 'id'> {}
 
 const bodyValidation: yup.ObjectSchema<IBodyProps> = yup.object().shape({
-    nome: yup.string().required().min(3),
+    nome: yup.string().required().min(3).max(150),
 });
 
 export const createValidation = validation({
@@ -15,6 +16,13 @@ export const createValidation = validation({
 });
 
 export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
-    console.log(req.body);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Not implemented');
+    const result = await CidadesProvider.create(req.body);
+    if(result instanceof Error){
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: result.message
+            }
+        });
+    }
+    return res.status(StatusCodes.CREATED).json(result);
 };
---
+++ src/server/database/providers/cidades/Create.ts
@@ -0,0 +1,20 @@
+import { ETableNames } from '../../ETableNames';
+import { Knex } from '../../knex';
+import { ICidade } from '../../models';
+
+export const create = async (cidade: Omit<ICidade, 'id'>): Promise<Number | Error> => {
+    try {
+        const [result] = await Knex(ETableNames.cidade).insert(cidade).returning('id');
+
+        if(typeof result === 'object'){
+            return result.id;
+        } else if (typeof result === 'number'){
+            return result;
+        }
+        return new Error(`Erro ao cadastrar ${ETableNames.cidade}.`);
+        
+    } catch (error) {
+        console.log('error= ' + error);
+        return new Error(`Erro ao cadastrar ${ETableNames.cidade}.`);
+    }
+};
